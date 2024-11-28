f0a7205a9291dd8bec8ab37728d7959781b93ecf
सिंकु कुमार <sudosinku@gmail.com>
2024-11-28 14:05:40 +0530

    Create basic_variables.js

---
+++ Basics/basic_variables.js
@@ -0,0 +1,17 @@
+let firstName = "Sinku";
+const lastName = "Kumar";
+var age = 25;
+
+console.log(typeof(age));
+console.log(firstName + " " + lastName);
+
+// Difference between let, const and var
+// let and const are block-scoped, while var is function-scoped
+// let and const are not hoisted, while var is hoisted
+// const is immutable, while let and var are mutable
+// const must be initialized during declaration, while let and var can be initialized later
+// const cannot be re-declared, while let and var can be re-declared
+// const is recommended for variables that should not be re-assigned
+// let is recommended for variables that should be re-assigned
+// var is not recommended in ES6
+
